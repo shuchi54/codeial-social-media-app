e435f0bcb0fe5c72a9e8829bf3268e4b5c1919b6
shuchi shukla <66742961+shuchi54@users.noreply.github.com>
2021-09-12 23:38:45 +0530

    error handling
    added flash messages

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,12 +1,48 @@
 const Post = require('../models/post')
+const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){console.log('error in creating a post'); return;}
+module.exports.create = async function(req, res){
 
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+       });
+    
+       if(req.xhr){
+           return res.status(200).json({
+               data: {
+                   post: post
+               },
+               message: "Post Created!"
+           });
+       }
+
+       req.flash('success', 'Post published!');
+       return res.redirect('back');
+
+    }catch(err){
+        req.flash('error', err);
         return res.redirect('back');
+    }
+
+ 
+}
+
+
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id, function(err, post){
+        // .id is in string for comparison
+         if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                req.flash('success', 'Post deleted');
+                return res.redirect('back');
+            });
+         }else{
+             return res.redirect('back');
+         }
+
     });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,10 +2,25 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+    User.findById(req.params.id, function(err, user){
+        return res.render('user_profile', {
+        title: 'User Profile',
+        profile_user: user
+    });
+    });
 }
+
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
+
 
 
 // render the sign up page
@@ -57,11 +72,13 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
+    req.flash('success', 'You have logged out');
 
     return res.redirect('/');
 }
